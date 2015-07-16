1917d1e2abe2a4302d6471c87bdab6c3d7e86796
Eric Wooley <Eric.Wooley@rosetta.com>
2015-07-16 16:57:23 -0700

    added firebase and torrent loading logic, with magic url. fixes #4

--- app/src/js/src/background.js
+++ app/src/js/src/background.js
@@ -1,7 +1,9 @@
 /* globals chrome */
 import WebTorrent from 'webtorrent';
+import Firebase from 'firebase';
+Firebase.INTERNAL.forceWebSockets();
 window.client = new WebTorrent();
-
+window.fb = new Firebase('https://torrentia.firebaseio.com/');
 chrome.app.runtime.onLaunched.addListener(() => {
   chrome.app.window.create('index.html', {
     bounds: {
--- app/src/js/src/stores/torrent-store.js
+++ app/src/js/src/stores/torrent-store.js
@@ -1,24 +1,79 @@
 import alt from '../alt';
 import TorrentActions from '../actions/torrent-actions';
-
+import md5 from 'md5';
+import blobToBuffer from 'blob-to-buffer';
+let fb = null;
 let client = null;
-
 function main(bg) {
-  client = bg.client
+  client = bg.client;
+  fb = bg.fb;
 }
+function fileNameFromURL(url) {
+  return url.split('\\').pop().split('/').pop();
+}
+
 chrome.runtime.getBackgroundPage(main);
 class TorrentStore {
   constructor() {
     this.bindListeners({
-      addTorrent: TorrentActions.addTorrent
+      addTorrentFromUrl: TorrentActions.addTorrent
     });
 
     this.state = {
-      torrents: [{test: 'test'}]
+      torrents: []
     };
   }
-  addTorrent({file}) {
-    this.state.torrents.push(file);
+  addTorrentFromUrl({url = 'https://www.petfinder.com/wp-content/uploads/2012/11/140272627-grooming-needs-senior-cat-632x475.jpg'}) {
+    const urlMD5 = md5(url);
+    fb.child(urlMD5).on('value', (snapshot) => {
+      const hashFromServer = snapshot.val();
+      if (hashFromServer) {
+        this.addTorrentFromHash({hashFromServer});
+      } else {
+        this.downloadUrlAsBlob({url, urlMD5});
+      }
+    }, (errorObject) => {
+      console.log('The read failed: ' + errorObject.code);
+    });
+
+  }
+  addTorrentFromHash({hash}) {
+    client.add(hash, (torrent) => {
+      this.state.torrents.push(torrent);
+    });
+  }
+  downloadUrlAsBlob({url, urlMD5}) {
+    const self = this;
+    const fileName = fileNameFromURL(url);
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'blob';
+    xhr.onload = function(e) {
+      if (this.status == 200) {
+        const myBlob = this.response;
+        self.seedBlob(myBlob, fileName, (torrentHash) => {
+          self.saveURLwithHash(urlMD5, torrentHash)
+        });
+      }
+    };
+    xhr.send();
+  }
+  saveURLwithHash(urlMD5, torrentHash) {
+    fb.child(urlMD5).set(torrentHash);
+  }
+  seedBlob(blob, fileName, cb) {
+    blobToBuffer (blob, (err, buffer) => {
+      if (err) {
+        throw err;
+      }
+      buffer.name = fileName;
+      client.seed(buffer, (torrent) => {
+        const torrentHash = torrent.infoHash;
+        console.log('torrentHash', torrent, torrentHash);
+        this.state.torrents.push(torrent);
+        cb(torrentHash);
+      })
+    });
   }
 }
 
